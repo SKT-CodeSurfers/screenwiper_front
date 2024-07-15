f4da3ceff8c7daba51bae9c942ac42c57b1b9687
daeun088 <kde2329@naver.com>
2024-07-15 18:47:49 +0900

    init : 폴더 구조 및 네비게이션 세팅

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from '../screens/Home';
+import DetailsScreen from '../screens/Details';
+
+export type RootStackParamList = {
+    Home: undefined;
+    Details: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const RootNavigator: React.FC = () => {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  };
+  
+  export default RootNavigator;
