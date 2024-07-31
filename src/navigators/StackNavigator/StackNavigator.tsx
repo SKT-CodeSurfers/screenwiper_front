b61040c78437318bebcedee0af0f9d05fcb1a8bc
daeun088 <kde2329@naver.com>
2024-08-01 02:44:52 +0900

    feat : SignIn UI

--- src/navigators/StackNavigator/StackNavigator.tsx
+++ src/navigators/StackNavigator/StackNavigator.tsx
@@ -6,14 +6,17 @@ import React from 'react';
 import LoadingScreen from '@/screens/Loading/LoadingScreen';
 import BottomTabNavigator from '@/navigators/BottomTabNavigator/BottomTabNavigator';
 import SplashScreen from '@/screens/Splash/SplashScreen';
+import SignInScreen from '@/screens/SignIn/SignInScreen';
 
 export type StackParamList = {
+  SignIn: undefined;
   Splash: undefined;
   Main: undefined;
   Loading: undefined;
 };
 
 export const enum StackMenu {
+  SignIn = 'SignIn',
   Splash = 'Splash',
   Main = 'Main',
   Loading = 'Loading',
@@ -27,6 +30,7 @@ export default function StackNavigator() {
   const Stack = createNativeStackNavigator<StackParamList>();
   return (
     <Stack.Navigator>
+      <Stack.Screen name={StackMenu.SignIn} component={SignInScreen} options={{ headerShown: false }} />
       <Stack.Screen name={StackMenu.Splash} component={SplashScreen} />
       <Stack.Screen name={StackMenu.Main} component={BottomTabNavigator} />
       <Stack.Screen name={StackMenu.Loading} component={LoadingScreen} />
