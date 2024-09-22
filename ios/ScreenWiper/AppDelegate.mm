915ccd56a2d2e85a11dd2e1f0ff36017753ee715
Yooshin Kim <yusinkim.or@gmail.com>
2024-09-22 17:02:52 +0900

    test : ios 네이티브 스크린샷 감지 테스트

--- ios/ScreenWiper/AppDelegate.mm
+++ ios/ScreenWiper/AppDelegate.mm
@@ -18,10 +18,25 @@
 
   bool didFinish = [super application:application didFinishLaunchingWithOptions:launchOptions];
 
+  
+  // 스크린샷 감지 노티피케이션 등록
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(userDidTakeScreenshot:)
+                                               name:UIApplicationUserDidTakeScreenshotNotification
+                                             object:nil];
+  
   [RNSplashScreen show];
   
   return didFinish;
 }
+
+// 스크린샷 감지 메소드
+- (void)userDidTakeScreenshot:(NSNotification *)notification
+{
+    NSLog(@"스크린샷이 감지되었습니다!");
+    // 스크린샷 발생 시 필요한 추가 작업을 여기서 처리
+}
+
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
