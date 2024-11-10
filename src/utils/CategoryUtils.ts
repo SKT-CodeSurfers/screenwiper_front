bb161ed56c1180dc86d67ed7d11d7f5e093f7206
daeun088 <kde2329@naver.com>
2024-11-10 15:14:01 +0900

    fix : 추천 API 형식 수정

--- src/utils/CategoryUtils.ts
+++ src/utils/CategoryUtils.ts
@@ -6,34 +6,37 @@ export const categorizeData = (rawData: GetRecommandResponse): TopTabNavigatorPr
   const planCards: CardItem[] = [];
   const otherCards: CardItem[] = [];
 
+  Object.entries(rawData).forEach(([key, items]) => {
+    if (Array.isArray(items)) {
+      items.forEach(item => {
+        if (item) {
+          const dateObject = item.date ? new Date(item.date) : null;
+          const day = dateObject ? dateObject.getDate() : '';
 
-  Object.entries(rawData).forEach(([key, item]) => {
-    if (item) {
-      const dateObject = item.date ? new Date(item.date) : null;
-      const day = dateObject ? dateObject.getDate() : '';
+          const card: CardItem = {
+            photoId: item.photo_id,
+            title: item.title,
+            address: item.address ?? "",
+            descriptions: item.summary ? [item.summary] : [],
+            category: item.category_name as '장소' | '일정' | '기타',
+            date: String(day),
+          };
 
-      const card: CardItem = {
-        photoId: item.photo_id,
-        title: item.title,
-        address: item.address ?? "",
-        descriptions: item.summary ? [item.summary] : [],
-        category: item.category_name as '장소' | '일정' | '기타',
-        date: String(day),
-      };
-
-      switch (item.category_name) {
-        case '장소':
-          placeCards.push(card);
-          break;
-        case '일정':
-          planCards.push(card);
-          break;
-        case '기타':
-          otherCards.push(card);
-          break;
-        default:
-          break;
-      }
+          switch (item.category_name) {
+            case '장소':
+              placeCards.push(card);
+              break;
+            case '일정':
+              planCards.push(card);
+              break;
+            case '기타':
+              otherCards.push(card);
+              break;
+            default:
+              break;
+          }
+        }
+      });
     }
   });
 
