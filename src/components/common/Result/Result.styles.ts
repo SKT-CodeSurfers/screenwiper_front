81ae54a74e2075b680d8adb4db58f05664334266
daeun088 <kde2329@naver.com>
2024-11-13 01:03:25 +0900

    feat : 이미지 검색 UI

--- src/components/common/Result/Result.styles.ts
+++ src/components/common/Result/Result.styles.ts
@@ -26,6 +26,14 @@ const ResultStyles = {
     padding-left: 5px;
     line-height: 25px;
   `,
+
+  Line: styled.View`
+    flex: 1;
+    border-bottom-width: 1px;
+    margin-vertical: 10px;
+    width: 100%;
+    border-bottom-color: ${({theme}) => theme.colors.gray2};
+  `,
 };
 
 const ResultPlanStyles = {
@@ -50,5 +58,43 @@ const ResultPlanStyles = {
   `,
 };
 
+const ResultSearch = {
+  SearchContainer: styled.View`
+    ${({theme}) => theme.mixins.flexBox('column', 'center', 'flex-start')}
+    gap: 10px;
+  `,
+
+  SearchBox: styled.View`
+    ${({theme}) => theme.mixins.flexBox('row', 'space-between', 'center')}
+    gap: 10px;
+    width: 100%;
+    padding: 20px;
+    border-radius: 8px;
+    background: ${({theme}) => theme.colors.gray2};
+    align-items: stretch;
+  `,
+
+  TextContainer: styled.View`
+    flex: 2;
+    ${({theme}) => theme.mixins.flexBox('column', 'flex-start', 'flex-start')}
+  `,
+
+  SearchTitle: styled.Text`
+    ${({theme}) => theme.fonts.body_m_15};
+  `,
+
+  SearchDate: styled.Text`
+    ${({theme}) => theme.fonts.body_m_12};
+    margin-top: 5px;
+  `,
+
+  SearchImage: styled.Image`
+    flex: 1;
+    height: 100%;
+    border-radius: 8px;
+  `,
+};
+
 export default ResultStyles;
 export {ResultPlanStyles};
+export {ResultSearch};
--- src/components/common/Result/ResultOther.tsx
+++ src/components/common/Result/ResultOther.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Result.styles';
+import {ResultSearch as SR} from './Result.styles';
 import {IResultOther} from '@/types/Result/ResultTypes';
 
 interface ResultOtherProps {
   data: IResultOther;
 }
 export default function ResultOther({data}: ResultOtherProps) {
+
+  // 임시 데이터 설정
+  const [plans, setPlans] = useState([
+    {
+      name: '서울 전시회',
+      date: '2024-11-03',
+      imageUrl: 'https://example.com/image1.jpg',
+    },
+    {
+      name: '강릉 여행',
+      date: '2024-12-10',
+      imageUrl: 'https://example.com/image2.jpg',
+    },
+    {
+      name: '부산 콘서트',
+      date: '2024-12-15',
+      imageUrl: 'https://example.com/image3.jpg',
+    },
+  ]);
+
   return (
     <>
       <s.SubWrapper isVertical={true}>
         <s.SubTitle>✨ 요약</s.SubTitle>
         <s.SummaryContent>{data.summary}</s.SummaryContent>
+        <s.Line/>
+        <s.SubTitle>🔎 이미지 검색결과</s.SubTitle>
+        <SR.SearchContainer>
+          {plans.map((plan, idx) => (
+            <SR.SearchBox key={idx}>
+              <SR.TextContainer>
+                <SR.SearchTitle numberOfLines={2} ellipsizeMode="tail">{plan.name}</SR.SearchTitle>
+                <SR.SearchDate>{plan.date}</SR.SearchDate>
+              </SR.TextContainer>
+              <SR.SearchImage source={{ uri: plan.imageUrl }} />
+            </SR.SearchBox>
+          ))}
+      </SR.SearchContainer>
       </s.SubWrapper>
     </>
   );
