9add977873cbbbd1390270747bf948ace12f3f95
Yooshin Kim <yusinkim.or@gmail.com>
2024-11-15 01:47:57 +0900

    feat : 검색 정보 노츨 UI 수정

--- src/components/common/Result/ResultOther.tsx
+++ src/components/common/Result/ResultOther.tsx
@@ -1,30 +1,39 @@
-import React, {useState} from 'react';
-import { TouchableOpacity, Linking, ActivityIndicator } from 'react-native';
+import React, {Suspense, useState} from 'react';
+import {
+  TouchableOpacity,
+  Linking,
+  ActivityIndicator,
+  FlatList,
+  Text,
+} from 'react-native';
 import s from './Result.styles';
 import {ResultSearch as SR} from './Result.styles';
 import {IResultOther} from '@/types/Result/ResultTypes';
-import {useGetDetect} from '@/hooks/queries/photos/useGetDetect'
+import {useGetDetect} from '@/hooks/queries/photos/useGetDetect';
 
 interface ResultOtherProps {
   data: IResultOther;
 }
 export default function ResultOther({data}: ResultOtherProps) {
-
   const {photoId} = data;
   const {data: detectData, isError, isLoading} = useGetDetect({photoId});
 
-  if (isLoading) {
-    return console.log("로딩중");
-  }
-
-  if (isError) {
-    return console.log("에러");
-  }
-
   const handleCardPress = (url: string) => {
     Linking.openURL(url).catch(err =>
-      console.error("URL을 여는 중 오류가 발생했습니다:", err)
+      console.error('URL을 여는 중 오류가 발생했습니다:', err),
     );
+  };
+
+  const [similarImages, setSimilarImages] = useState(
+    detectData?.visuallySimilarImages,
+  );
+  const recommendImages = detectData?.pagesWithMatchingImages;
+
+  const handleImageError = imageUrl => {
+    setSimilarImages(prevImages =>
+      prevImages.filter(image => image.imageUrl !== imageUrl),
+    );
+    console.log('이미지 로드 실패:', imageUrl);
   };
 
   return (
@@ -32,29 +41,75 @@ export default function ResultOther({data}: ResultOtherProps) {
       <s.SubWrapper isVertical={true}>
         <s.SubTitle>✨ 요약</s.SubTitle>
         <s.SummaryContent>{data.summary}</s.SummaryContent>
-        <s.Line/>
-        <s.SubTitle>🔎 이미지 검색결과</s.SubTitle>
-        <SR.SearchContainer>
-          {detectData?.similarImages?.map((imageUrl, idx)=> (
-            <TouchableOpacity key={idx} onPress={() => handleCardPress(imageUrl)}>
-              <SR.SearchBox>
-                <SR.TextContainer>
-                  <SR.SearchTitle numberOfLines={2} ellipsizeMode="tail">
-                    이미지 {idx + 1}
-                  </SR.SearchTitle>
-                </SR.TextContainer>
-                {imageUrl ? (
-                  <SR.SearchImage
-                    source={{ uri: imageUrl }}
-                    onError={() => console.log('이미지 로드 실패:', imageUrl)}
-                  />
-                ) : (
-                  <ActivityIndicator size="small" color="#0000ff" />
-                )}
-              </SR.SearchBox>
-            </TouchableOpacity>
-          ))}
-      </SR.SearchContainer>
+        <s.Line />
+
+        <SR.SearchTitleText>검색 정보</SR.SearchTitleText>
+
+        {isLoading ? (
+          <ActivityIndicator size="small" />
+        ) : (
+          <>
+            {!(similarImages && similarImages.length > 0) &&
+              !(recommendImages && recommendImages.length > 0) && (
+                <s.SummaryContent>검색 결과가 없어요</s.SummaryContent>
+              )}
+            {similarImages && similarImages.length > 0 && (
+              <>
+                <s.SubTitle>🔎 유사한 이미지</s.SubTitle>
+                <SR.SearchBoxContainer
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  contentContainerStyle={{
+                    columnGap: 10,
+                  }}>
+                  {similarImages.map((item, idx) => (
+                    <TouchableOpacity
+                      key={idx}
+                      onPress={() => handleCardPress(item.imageUrl)}>
+                      {item.imageUrl && (
+                        <SR.SearchImage
+                          source={{uri: item.imageUrl}}
+                          onError={() => handleImageError(item.imageUrl)}
+                        />
+                      )}
+                    </TouchableOpacity>
+                  ))}
+                </SR.SearchBoxContainer>
+                <s.Line />
+              </>
+            )}
+            {recommendImages && recommendImages.length > 0 && (
+              <>
+                <s.SubTitle>🔎 유사한 페이지</s.SubTitle>
+                <SR.SearchContainer>
+                  {recommendImages?.map(({imageUrl, url, title}, idx) => (
+                    <TouchableOpacity
+                      key={idx}
+                      onPress={() => handleCardPress(url)}>
+                      <SR.SearchItem>
+                        <SR.TextContainer>
+                          <SR.SearchTitle
+                            numberOfLines={2}
+                            ellipsizeMode="tail">
+                            {title}
+                          </SR.SearchTitle>
+                        </SR.TextContainer>
+                        {imageUrl && (
+                          <SR.SearchImage
+                            source={{uri: imageUrl}}
+                            onError={() =>
+                              console.log('이미지 로드 실패:', imageUrl)
+                            }
+                          />
+                        )}
+                      </SR.SearchItem>
+                    </TouchableOpacity>
+                  ))}
+                </SR.SearchContainer>
+              </>
+            )}
+          </>
+        )}
       </s.SubWrapper>
     </>
   );
--- src/hooks/queries/photos/useGetDetect.ts
+++ src/hooks/queries/photos/useGetDetect.ts
@@ -1,17 +1,23 @@
-import { GET } from '@/api/client';
-import { useQuery } from '@tanstack/react-query';
+import {GET} from '@/api/client';
+import {useQuery} from '@tanstack/react-query';
 
 // 요청 타입 정의
 export interface GetDetectRequest {
   photoId: string;
 }
 
+type Image = {
+  title: string | null;
+  imageUrl: string;
+  url?: string;
+};
 export interface GetDetectResponse {
-  similarImages: string[];
+  pagesWithMatchingImages: Image[];
+  visuallySimilarImages: Image[];
 }
 
 // 이미지 분석 API 호출 함수
-export const getDetect = async ({ photoId }: GetDetectRequest) => {
+export const getDetect = async ({photoId}: GetDetectRequest) => {
   const res = await GET<GetDetectResponse>(`/images/detect/${photoId}`);
   console.log('>>', res.data);
   return res.data;
@@ -19,11 +25,11 @@ export const getDetect = async ({ photoId }: GetDetectRequest) => {
 
 export const useGetDetect = (req: GetDetectRequest) => {
   const QUERY_KEY = ['PHOTOS', 'DETECT', req.photoId];
-  
-  const { data, isLoading, isError, refetch } = useQuery({
+
+  const {data, isLoading, isError, refetch} = useQuery({
     queryKey: QUERY_KEY,
     queryFn: () => getDetect(req),
   });
 
-  return { data, isLoading, isError, refetch };
+  return {data, isLoading, isError, refetch};
 };
